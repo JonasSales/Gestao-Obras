fa0ad681c7b18fb524c8f85ea45c06dbd9dcfa62
jonas <jonassales510@gmail.com>
2025-08-06 14:51:17 -0300

    Inicialização básica de projeto.

---
+++ src/main/java/br/com/gestao_obras/config/JwtAuthenticationFilter.java
@@ -0,0 +1,59 @@
+package br.com.gestao_obras.config;
+
+import br.com.gestao_obras.repository.UserRepository;
+import br.com.gestao_obras.service.CustomUserDetailsService;
+import br.com.gestao_obras.service.JwtUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+    private final UserRepository userRepository;
+
+    public JwtAuthenticationFilter(JwtUtil jwtUtil, CustomUserDetailsService userDetailsService,  UserRepository userRepository) {
+        this.jwtUtil = jwtUtil;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws IOException, ServletException {
+        try {
+            String token = this.recoverToken(request);
+            if (token != null) {
+                String subject = jwtUtil.validateToken(token);
+                UserDetails userDetails = userRepository.findByEmail(subject).orElseThrow();
+
+                var authentication = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities()
+                );
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+
+            filterChain.doFilter(request, response);
+
+        } catch (Exception e) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write("Unauthorized: " + e.getMessage());
+        }
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            return authHeader.substring(7);
+        }
+        return null;
+    }
+}
---
+++ src/main/java/br/com/gestao_obras/service/CustomUserDetailsService.java
@@ -0,0 +1,39 @@
+package br.com.gestao_obras.service;
+
+
+
+import br.com.gestao_obras.model.User;
+import br.com.gestao_obras.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepo;
+
+    public CustomUserDetailsService(UserRepository userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User u = userRepo.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(u.getUsername())
+                .password(u.getPassword())
+                .authorities(
+                        (GrantedAuthority) u.getRoles().stream()
+                                .map(r -> new SimpleGrantedAuthority(r.getName().name()))
+                                .collect(Collectors.toList())
+                )
+                .build();
+    }
+
+}
