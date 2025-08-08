51fed4f3cfa8855b077e8f1eacec286f1db93b9e
jonas <jonassales510@gmail.com>
2025-08-08 09:26:58 -0300

    add: @Data annotation

--- src/main/java/br/com/gestao_obras/dto/Response/UserRegisterResponse.java
+++ src/main/java/br/com/gestao_obras/dto/Response/UserRegisterResponse.java
@@ -1,15 +1,12 @@
 package br.com.gestao_obras.dto.Response;
 
 import br.com.gestao_obras.model.User;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
+
+@Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class UserRegisterResponse {
     private String firstName;
     private String lastName;
