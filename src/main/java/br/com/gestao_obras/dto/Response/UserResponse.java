04c77668f3b69178e82b87dc520db834291b23fa
jonas <jonassales510@gmail.com>
2025-08-08 10:50:44 -0300

    update: renaming to UserResponse

--- src/main/java/br/com/gestao_obras/dto/Response/UserResponse.java
+++ src/main/java/br/com/gestao_obras/dto/Response/UserResponse.java
@@ -1,18 +1,19 @@
 package br.com.gestao_obras.dto.Response;
 
 import br.com.gestao_obras.model.User;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
 @Data
 @NoArgsConstructor
-public class UserRegisterResponse {
+@AllArgsConstructor
+public class UserResponse {
     private String firstName;
     private String lastName;
     private String email;
 
-    public  UserRegisterResponse(User user) {
+    public UserResponse(User user) {
         this.firstName = user.getFirstName();
         this.lastName = user.getLastName();
         this.email = user.getEmail();
