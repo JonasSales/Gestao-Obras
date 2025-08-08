5265f0c19388cce012d28a7000f02c30d045ad6a
jonas <jonassales510@gmail.com>
2025-08-08 09:27:17 -0300

    add: Creating DTO to Update

---
+++ src/main/java/br/com/gestao_obras/dto/Request/UpdateResquest.java
@@ -0,0 +1,9 @@
+package br.com.gestao_obras.dto.Request;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class UpdateResquest {
+}
---
+++ src/main/java/br/com/gestao_obras/dto/Response/UpdateResponse.java
@@ -0,0 +1,9 @@
+package br.com.gestao_obras.dto.Response;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class UpdateResponse {
+}
