5a282c21a05fca03f7d8914385bee1661340fe21
jonas <jonassales510@gmail.com>
2025-08-08 09:26:38 -0300

    add: @Data annotation

--- src/main/java/br/com/gestao_obras/model/User.java
+++ src/main/java/br/com/gestao_obras/model/User.java
@@ -3,18 +3,15 @@ package br.com.gestao_obras.model;
 import br.com.gestao_obras.dto.Request.RegisterRequest;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-    import org.springframework.security.core.GrantedAuthority;
-    import org.springframework.security.core.userdetails.UserDetails;
-
-    import java.util.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.*;
 
 @Entity
 @Table(name = "users")
-@Getter
-@Setter
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class User implements UserDetails {
