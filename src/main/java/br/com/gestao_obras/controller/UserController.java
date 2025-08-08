4bfa284e55618a4eb80afa29fb49b25eef3e1e21
jonas <jonassales510@gmail.com>
2025-08-08 10:57:21 -0300

    add: Update and Delete Methods

--- src/main/java/br/com/gestao_obras/controller/UserController.java
+++ src/main/java/br/com/gestao_obras/controller/UserController.java
@@ -1,21 +1,36 @@
 package br.com.gestao_obras.controller;
 
-import br.com.gestao_obras.dto.Response.UpdateResponse;
+import br.com.gestao_obras.dto.Request.UpdateResquest;
+import br.com.gestao_obras.dto.Response.UserResponse;
+import br.com.gestao_obras.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
 
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<UserResponse> getUser(@AuthenticationPrincipal UserDetails userDetails) {
+        return userService.getProfile(userDetails);
+    }
 
     @PutMapping()
-    public ResponseEntity<UpdateResponse> updateUser(Authentication authentication) {
-        System.out.println("Fast test to authentication" + authentication.getName());
-
-        return  ResponseEntity.accepted().build();
+    public ResponseEntity<UserResponse> updateUser(@AuthenticationPrincipal UserDetails userDetails,
+                                                   @Valid @RequestBody UpdateResquest updateResquest) {
+        return userService.update(userDetails, updateResquest);
+    }
+    @DeleteMapping
+    public ResponseEntity<UserResponse> deleteUser(@AuthenticationPrincipal UserDetails userDetails) {
+        return userService.delete(userDetails);
     }
 }
